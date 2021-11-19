0df8bdd75dbaa0ee6d08a4761ff7238b55c3b086
Aurélien Ponte <aurelien.junk@gmail.com>
2021-11-19 22:47:43 +0100

    interesting update to add dummy dimensions

--- sstats/tseries.py
+++ sstats/tseries.py
@@ -20,12 +20,14 @@ import statsmodels.api as sm
 
 def wrapper(func,
             time,
-            params=1,
+            params=None,
+            dummy_dims=dict(draw=1),
             chunks=None,
             dtype='float',
             output='xarray',
             name='z',
-            **kwargs):
+            **kwargs,
+           ):
     """ Wraps timeseries generation code in order to distribute the generation
 
     Parameters
@@ -38,7 +40,10 @@ def wrapper(func,
             Number of time steps, time array, tuple (T, dt)
         params: int, dict, optional
             Parameters that will lead to dimensions or required to generate
-            time series
+            time series.
+            Each parameter can be a float, a list or a numpy array.
+        dummy_dims: dict
+            Dummy (extra) dimensions, e.g. dummy_dims={"draw": 10}
         chunks: dict, optional
             Associated chunks
         seed: int, optional
@@ -58,16 +63,14 @@ def wrapper(func,
         time = np.array(time)
     Nt = time.size
 
+    dims = {}
     if isinstance(params, dict):
-        dims = {}
         for d, v in params.items():
-            if d=='draw' and isinstance(v, int):
-                dims[d] = np.arange(v)
-            else:
-                dims[d] = np.array(v, ndmin=1)
-    else:
-        dims = {'draw': np.array(range(params), ndmin=1)}
+            dims[d] = np.array(v, ndmin=1)
     dims['time'] = time
+    # dummy dimensions are last, just after the time dimension
+    for d, v in dummy_dims.items():
+        dims[d] = np.arange(v)
     Nd = len(dims)
     shape = tuple(v.size for d, v in dims.items())
 
@@ -81,27 +84,25 @@ def wrapper(func,
     # Note: adding name to dimension names below is pretty critical if
     #   multiple calls to wrapper are made.
     #   dask will create a single object ... danger
-    dims_da = tuple(da.from_array(dims[d]
+    dims_da = [da.from_array(dims[d]
                                   .reshape(tuple(dims[d].size if i==j else 1 for j in range(Nd))),
                                   chunks=tuple(xr_chunks[d] if i==j else -1 for j in range(Nd)),
                                   name=name+d
                                  )
                      for i, d in enumerate(dims)
-                    )
+              ]
 
     # wraps func to reinit numpy seed from chunk number
     def _func(*args, seed=None, block_info=None, **kwargs):
         if seed is None:
             seed = np.random.randint(0,2**32-1)
         np.random.seed(seed+block_info[0]['num-chunks'][0])
-        return func(*args[1:],
-                    draws=args[0].shape[-2],
+        return func(*args[1:], # skips first, because it is the final array
                     seed=seed,
                     **kwargs)
 
     x = da.empty(shape=shape, chunks=da_chunks)
-    dims_da = tuple(d for d in dims_da if d.name!=name+'draw')
-    x = x.map_blocks(_func, *dims_da, **kwargs, dtype=dtype)
+    x = da.map_blocks(_func, x, *dims_da, **kwargs, dtype=dtype)
     x = x.squeeze()
     dims = {d: v for d, v in dims.items() if v.size>1}
 
@@ -127,24 +128,44 @@ def wrapper(func,
 
     return x
 
-def _uniform(low, high, time, draws=1, seed=None, **kwargs):
+def _uniform(low, high, time, *args, seed=None, **kwargs):
+    """ atomic wrapper around uniform distribution method
+    """
     rng = np.random.default_rng(seed=seed)
     if not isinstance(time, int):
         time = time.size
-    out = np.zeros((low.size, high.size, draws, time))
-    for i, l in enumerate(low[:,0,0,0]):
-        for j, h in enumerate(high[0,:,0,0]):
-            out[i,j,:,:] = rng.uniform(l, h, (draws, time))
+    extra = (time,) + tuple(a.size for a in args)
+    out = np.zeros((low.size, high.size,)+extra)
+    for i, l in enumerate(low.flatten()):
+        for j, h in enumerate(high.flatten()):
+            out[i,j,:,:] = rng.uniform(l, h, extra)
     return out
 
-def uniform(time, low=0., high=1., draws=1, **kwargs):
-    """ wraps numpy random methods
+def uniform(time, low=0., high=1., draws=1, dummy_dims=None, **kwargs):
+    """ Generates uniformly distributed numbers
+
+    Parameters
+    ----------
+    time: int, np.ndarray, tuple
+        Number of time steps, time array, tuple (T, dt)
+    low, high: float, list, np.array, optional
+        Lower and upper bounds.
+        Default values are 0 and 1 respectively
+    draws: int
+        Number of random realizations
+
+    References
+    ----------
     https://numpy.org/doc/stable/reference/random/index.html#quick-start
     https://docs.python.org/dev/library/random.html#random.random
     """
+    _dummy_dims = dict(draw=draws)
+    if dummy_dims is not None:
+        _dummy_dims.update(**dummy_dims)
     x = wrapper(_uniform,
                 time,
-                params={'low': low, 'high': high, 'draw': draws},
+                params={'low': low, 'high': high},
+                dummy_dims=_dummy_dims,
                 **kwargs)
     if 'low' not in x.dims:
         x = x.assign_attrs(low=low)
@@ -152,49 +173,88 @@ def uniform(time, low=0., high=1., draws=1, **kwargs):
         x = x.assign_attrs(high=high)
     return x
 
-def _normal(loc, scale, time, draws=1, seed=None, **kwargs):
+def _normal(loc, scale, time, *args, seed=None, **kwargs): #
+    """ atomic wrapper around normal distribution method
+    """
     rng = np.random.default_rng(seed=seed)
     if not isinstance(time, int):
         time = time.size
-    out = np.zeros((loc.size, scale.size, draws, time))
-    for i, l in enumerate(loc[:,0,0,0]):
-        for j, s in enumerate(scale[0,:,0,0]):
-            out[i,j,:,:] = rng.normal(l, s, (draws, time))
+    extra = (time,) + tuple(a.size for a in args)
+    out = np.zeros((loc.size, scale.size)+extra)
+    for i, l in enumerate(loc.flatten()):
+        for j, s in enumerate(scale.flatten()):
+            out[i, j, ...] = rng.normal(l, s, extra)
     return out
 
-def normal(time, loc=0., scale=1., draws=1, **kwargs):
-    """ wraps numpy random methods
+def normal(time, loc=0., scale=1., draws=1, dummy_dims=None, **kwargs):
+    """ Generates normally distributed numbers
+
+    Parameters
+    ----------
+    time: int, np.ndarray, tuple
+        Number of time steps, time array, tuple (T, dt)
+    loc, scale: float, list, np.array, optional
+        Mean and standard deviation of the distribution
+        Default values are 0 and 1 respectively
+    draws: int, optional
+        Number of random realizations
+    dummy_dims: dict
+        Additional dummy dimensions
+
     https://numpy.org/doc/stable/reference/random/index.html#quick-start
     https://docs.python.org/dev/library/random.html#random.random
     """
+    _dummy_dims = dict(draw=draws)
+    if dummy_dims is not None:
+        _dummy_dims.update(**dummy_dims)
     x = wrapper(_normal,
                 time,
-                params={'loc': loc, 'scale': scale, 'draw': draws},
-                **kwargs)
+                params={'loc': loc, 'scale': scale},
+                dummy_dims=_dummy_dims,
+                **kwargs,
+               )
     if 'loc' not in x.dims:
         x = x.assign_attrs(loc=loc)
     if 'scale' not in x.dims:
         x = x.assign_attrs(scale=scale)
     return x
 
-def _binomial(n, p, time, draws=1, seed=None, **kwargs):
+def _binomial(n, p, time, *args, seed=None, **kwargs):
+    """ atomic wrapper around binomial distribution method
+    """
     rng = np.random.default_rng(seed=seed)
     if not isinstance(time, int):
         time = time.size
-    out = np.zeros((n.size, p.size, draws, time))
-    for i, _n in enumerate(n[:,0,0,0]):
-        for j, _p in enumerate(p[0,:,0,0]):
-            out[i,j,:,:] = rng.binomial(_n, _p, (draws, time))
+    extra = (time,) + tuple(a.size for a in args)
+    out = np.zeros((n.size, p.size,)+extra)
+    for i, _n in enumerate(n.flatten()):
+        for j, _p in enumerate(p.flatten()):
+            out[i,j,:,:] = rng.binomial(_n, _p, extra)
     return out
 
-def binomial(time, n=1, p=.5, draws=1, **kwargs):
-    """ wraps numpy random methods
+def binomial(time, n=1, p=.5, draws=1, dummy_dims=None, **kwargs):
+    """ Generates binomial random numbers
+
+    Parameters
+    ----------
+    time: int, np.ndarray, tuple
+        Number of time steps, time array, tuple (T, dt)
+    n, p: float, list, np.array, optional
+        Parameters of binomial distribution (see numpy doc)
+        Default values are 1 and 0.5 respectively
+    draws: int
+        Number of random realizations
+
     https://numpy.org/doc/stable/reference/random/index.html#quick-start
     https://docs.python.org/dev/library/random.html#random.random
     """
+    _dummy_dims = dict(draw=draws)
+    if dummy_dims is not None:
+        _dummy_dims.update(**dummy_dims)
     x = wrapper(_binomial,
                 time,
-                params={'n': n, 'p': p, 'draw': draws},
+                params={'n': n, 'p': p},
+                dummy_dims=_dummy_dims,
                 **kwargs)
     if 'n' not in x.dims:
         x = x.assign_attrs(n=n)
@@ -202,26 +262,27 @@ def binomial(time, n=1, p=.5, draws=1, **kwargs):
         x = x.assign_attrs(p=p)
     return x
 
-def _exp_autocorr(T, rms, time, draws=1, dt=None, seed=None, **kwargs):
+def _exp_autocorr(T, rms, time, *args, dt=None, seed=None, **kwargs):
     """ exp_autocorr core code. See exp_autocorr doc
     """
     #time = float(time.squeeze())
     np.random.seed(seed)
     arma = sm.tsa.arma_generate_sample
-    out = np.zeros((T.size, rms.size, draws, time.size))
-    for i, t in enumerate(T[:,0,0,0]):
-        for j, r in enumerate(rms[0,:,0,0]):
+    extra = (time,) + tuple(a.size for a in args)
+    out = np.zeros((T.size, rms.size,)+extra)
+    for i, t in enumerate(T.flatten()):
+        for j, r in enumerate(rms.flatten()):
             ar = np.array([1, -1+dt/t]) # watch for sign
             am = np.array([1,])
-            out[i,j,:,:] = arma(ar,
+            out[i,j,...] = arma(ar,
                                 am,
-                                (draws, time.size),
-                                axis=-1,
+                                extra,
+                                axis=0,
                                 scale=np.sqrt(2*dt/t)*r,
                                )
     return out
 
-def exp_autocorr(time, T, rms, draws=1, **kwargs):
+def exp_autocorr(time, T, rms, draws=1, dummy_dims=None, **kwargs):
     """Generate exponentially correlated time series
     Implemented via ARMA
     x_{t} = x_{t-1} * (1-dt/T) + \sqrt{2*dt/T}*rms *e_t
@@ -239,6 +300,10 @@ def exp_autocorr(time, T, rms, draws=1, **kwargs):
     seed: int, optional
         numpy seed
     """
+    _dummy_dims = dict(draw=draws)
+    if dummy_dims is not None:
+        _dummy_dims.update(**dummy_dims)
+
     if isinstance(time, tuple):
         dt = time[1]
     else:
@@ -246,7 +311,8 @@ def exp_autocorr(time, T, rms, draws=1, **kwargs):
 
     x = wrapper(_exp_autocorr,
                 time,
-                params={'T': T, 'rms': rms, 'draw': draws},
+                params={'T': T, 'rms': rms,},
+                dummy_dims=_dummy_dims,
                 dt=dt,
                 **kwargs)
     if 'T' not in x.dims:
